4f7612ad96e147faeec92a55396a14cfbd0504f2
Trương Hải Đăng <haidanghth910@gmail.com>
2025-10-03 20:27:50 +0700

    Update setup script to include upgrades and new packages

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo apt-get update
+sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y build-essential cmake g++ libboost-all-dev libnotify-bin
 sudo apt install libzstd-dev -y
 sudo apt install -y libnghttp2-dev
+sudo apt-get install -y libssl-dev pkg-config
